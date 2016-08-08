13dfb5dcb3b65cdc90b805d271f94a93872c7bcf
Guardado <jgarzagu@ucsd.esu>
2016-08-07 23:11:03 -0700

    Adding missing autotools files

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Before runinning this script you need to install:
+# 	- autotools (sudo apt-get install autotools-dev)
+#
+# I know I could do better in this script, srry
+
+echo "Running autoreconf -i"
+autoreconf -i
